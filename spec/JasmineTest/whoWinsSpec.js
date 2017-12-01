544e0a7d0206859516abf70c27de212e1e8fefcf
Pablo <paul.ladiv123@gmail.com>
2017-12-01 14:54:39 +0000

    add Class WhoWins with declareWinner method + separate spec file

---
+++ spec/JasmineTest/whoWinsSpec.js
@@ -0,0 +1,17 @@
+describe("WhoWins", function() {
+
+      var WhoWins = require('../../lib/js/whowins');
+      var whoWins, gameScoreArrWins, gameScoreArrLoses;
+
+      beforeEach(function() {
+          whoWins = new WhoWins();
+          gameScoreArrWins = ["#1", "#2", "#3"];
+          gameScoreArrLoses = ["#1", "#2", "#4"];
+
+        });
+
+        it("return true when Win ", function() {
+          expect(whoWins.declareWinner(gameScoreArrWins)).toEqual(true);
+        });
+
+      });
